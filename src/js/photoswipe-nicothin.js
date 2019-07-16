c7162349f9eb5420728003b1d5c09206acbd2b16
Nikolay Gromov <nicothin@gmail.com>
2019-07-17 00:37:15 +0300

    Фотосвайпер: выпилил анимацию миниатюры в большое фото

--- src/js/photoswipe-nicothin.js
+++ src/js/photoswipe-nicothin.js
@@ -2564,7 +2564,7 @@ var _showOrHideTimeout,
     };
 
     // if bounds aren't provided, just open gallery without animation
-    if(!duration || !thumbBounds || thumbBounds.x === undefined) {
+    // if(!duration || !thumbBounds || thumbBounds.x === undefined) {
 
       _shout('initialZoom' + (out ? 'Out' : 'In') );
 
@@ -2583,110 +2583,110 @@ var _showOrHideTimeout,
         onComplete();
       }
 
-      return;
-    }
+      // return;
+    // }
 
-    var startAnimation = function() {
-      var closeWithRaf = _closedByScroll,
-        fadeEverything = !self.currItem.src || self.currItem.loadError || _options.showHideOpacity;
-
-      // apply hw-acceleration to image
-      if(item.miniImg) {
-        item.miniImg.style.webkitBackfaceVisibility = 'hidden';
-      }
-
-      if(!out) {
-        _currZoomLevel = thumbBounds.w / item.w;
-        _panOffset.x = thumbBounds.x;
-        _panOffset.y = thumbBounds.y - _initalWindowScrollY;
-
-        self[fadeEverything ? 'template' : 'bg'].style.opacity = 0.001;
-        _applyCurrentZoomPan();
-      }
-
-      _registerStartAnimation('initialZoom');
-
-      if(out && !closeWithRaf) {
-        framework.removeClass(template, 'pswp--animated-in');
-      }
-
-      if(fadeEverything) {
-        if(out) {
-          framework[ (closeWithRaf ? 'remove' : 'add') + 'Class' ](template, 'pswp--animate_opacity');
-        } else {
-          setTimeout(function() {
-            framework.addClass(template, 'pswp--animate_opacity');
-          }, 30);
-        }
-      }
-
-      _showOrHideTimeout = setTimeout(function() {
-
-        _shout('initialZoom' + (out ? 'Out' : 'In') );
-
-
-        if(!out) {
-
-          // "in" animation always uses CSS transitions (instead of rAF).
-          // CSS transition work faster here,
-          // as developer may also want to animate other things,
-          // like ui on top of sliding area, which can be animated just via CSS
-
-          _currZoomLevel = item.initialZoomLevel;
-          _equalizePoints(_panOffset,  item.initialPosition );
-          _applyCurrentZoomPan();
-          _applyBgOpacity(1);
-
-          if(fadeEverything) {
-            template.style.opacity = 1;
-          } else {
-            _applyBgOpacity(1);
-          }
-
-          _showOrHideTimeout = setTimeout(onComplete, duration + 20);
-        } else {
-
-          // "out" animation uses rAF only when PhotoSwipe is closed by browser scroll, to recalculate position
-          var destZoomLevel = thumbBounds.w / item.w,
-            initialPanOffset = {
-              x: _panOffset.x,
-              y: _panOffset.y
-            },
-            initialZoomLevel = _currZoomLevel,
-            initalBgOpacity = _bgOpacity,
-            onUpdate = function(now) {
-
-              if(now === 1) {
-                _currZoomLevel = destZoomLevel;
-                _panOffset.x = thumbBounds.x;
-                _panOffset.y = thumbBounds.y  - _currentWindowScrollY;
-              } else {
-                _currZoomLevel = (destZoomLevel - initialZoomLevel) * now + initialZoomLevel;
-                _panOffset.x = (thumbBounds.x - initialPanOffset.x) * now + initialPanOffset.x;
-                _panOffset.y = (thumbBounds.y - _currentWindowScrollY - initialPanOffset.y) * now + initialPanOffset.y;
-              }
-
-              _applyCurrentZoomPan();
-              if(fadeEverything) {
-                template.style.opacity = 1 - now;
-              } else {
-                _applyBgOpacity( initalBgOpacity - now * initalBgOpacity );
-              }
-            };
-
-          if(closeWithRaf) {
-            _animateProp('initialZoom', 0, 1, duration, framework.easing.cubic.out, onUpdate, onComplete);
-          } else {
-            onUpdate(1);
-            _showOrHideTimeout = setTimeout(onComplete, duration + 20);
-          }
-        }
-
-      }, out ? 25 : 90); // Main purpose of this delay is to give browser time to paint and
-          // create composite layers of PhotoSwipe UI parts (background, controls, caption, arrows).
-          // Which avoids lag at the beginning of scale transition.
-    };
-    startAnimation();
+//     var startAnimation = function() {
+//       var closeWithRaf = _closedByScroll,
+//         fadeEverything = !self.currItem.src || self.currItem.loadError || _options.showHideOpacity;
+//
+//       // apply hw-acceleration to image
+//       if(item.miniImg) {
+//         item.miniImg.style.webkitBackfaceVisibility = 'hidden';
+//       }
+//
+//       if(!out) {
+//         _currZoomLevel = thumbBounds.w / item.w;
+//         _panOffset.x = thumbBounds.x;
+//         _panOffset.y = thumbBounds.y - _initalWindowScrollY;
+//
+//         self[fadeEverything ? 'template' : 'bg'].style.opacity = 0.001;
+//         _applyCurrentZoomPan();
+//       }
+//
+//       _registerStartAnimation('initialZoom');
+//
+//       if(out && !closeWithRaf) {
+//         framework.removeClass(template, 'pswp--animated-in');
+//       }
+//
+//       if(fadeEverything) {
+//         if(out) {
+//           framework[ (closeWithRaf ? 'remove' : 'add') + 'Class' ](template, 'pswp--animate_opacity');
+//         } else {
+//           setTimeout(function() {
+//             framework.addClass(template, 'pswp--animate_opacity');
+//           }, 30);
+//         }
+//       }
+//
+//       _showOrHideTimeout = setTimeout(function() {
+//
+//         _shout('initialZoom' + (out ? 'Out' : 'In') );
+//
+//
+//         if(!out) {
+//
+//           // "in" animation always uses CSS transitions (instead of rAF).
+//           // CSS transition work faster here,
+//           // as developer may also want to animate other things,
+//           // like ui on top of sliding area, which can be animated just via CSS
+//
+//           _currZoomLevel = item.initialZoomLevel;
+//           _equalizePoints(_panOffset,  item.initialPosition );
+//           _applyCurrentZoomPan();
+//           _applyBgOpacity(1);
+//
+//           if(fadeEverything) {
+//             template.style.opacity = 1;
+//           } else {
+//             _applyBgOpacity(1);
+//           }
+//
+//           _showOrHideTimeout = setTimeout(onComplete, duration + 20);
+//         } else {
+//
+//           // "out" animation uses rAF only when PhotoSwipe is closed by browser scroll, to recalculate position
+//           var destZoomLevel = thumbBounds.w / item.w,
+//             initialPanOffset = {
+//               x: _panOffset.x,
+//               y: _panOffset.y
+//             },
+//             initialZoomLevel = _currZoomLevel,
+//             initalBgOpacity = _bgOpacity,
+//             onUpdate = function(now) {
+//
+//               if(now === 1) {
+//                 _currZoomLevel = destZoomLevel;
+//                 _panOffset.x = thumbBounds.x;
+//                 _panOffset.y = thumbBounds.y  - _currentWindowScrollY;
+//               } else {
+//                 _currZoomLevel = (destZoomLevel - initialZoomLevel) * now + initialZoomLevel;
+//                 _panOffset.x = (thumbBounds.x - initialPanOffset.x) * now + initialPanOffset.x;
+//                 _panOffset.y = (thumbBounds.y - _currentWindowScrollY - initialPanOffset.y) * now + initialPanOffset.y;
+//               }
+//
+//               _applyCurrentZoomPan();
+//               if(fadeEverything) {
+//                 template.style.opacity = 1 - now;
+//               } else {
+//                 _applyBgOpacity( initalBgOpacity - now * initalBgOpacity );
+//               }
+//             };
+//
+//           if(closeWithRaf) {
+//             _animateProp('initialZoom', 0, 1, duration, framework.easing.cubic.out, onUpdate, onComplete);
+//           } else {
+//             onUpdate(1);
+//             _showOrHideTimeout = setTimeout(onComplete, duration + 20);
+//           }
+//         }
+//
+//       }, out ? 25 : 90); // Main purpose of this delay is to give browser time to paint and
+//           // create composite layers of PhotoSwipe UI parts (background, controls, caption, arrows).
+//           // Which avoids lag at the beginning of scale transition.
+//     };
+//     startAnimation();
 
   };
 
